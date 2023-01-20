a1489d2b08f17c345f0feefa19e2ac60e1b4a75f
Alex Logacho <alexpaul200415@gmail.com>
2023-01-20 17:18:22 -0500

    TAREA

---
+++ Main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){                
+    
+    FILE *archivo=NULL;
+    FILE *archivo2=NULL;
+    int num;
+
+    archivo=fopen("vector.txt","r");
+    archivo2=fopen("vector2.txt","w");
+
+    if (archivo==NULL){
+        printf("No se puede abrir el archivo");
+        return -1;
+    }
+    for(int i=0; i<100; i++){
+        fprintf(archivo,"%d\n",i);
+    }
+    while(!feof(archivo)){
+        fscanf(archivo, "%d", &num);
+    if(num%3==0){
+        fprintf(archivo,"multiplo de 3");
+    }
+    else{
+        fprintf(archivo2,"%d\n",num);
+    }
+    }
+    fclose(archivo);
+    fclose(archivo2);
+     return 0;
+}
+
+
+
+
+
